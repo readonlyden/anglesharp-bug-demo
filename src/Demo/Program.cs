f969749f4f9319d072e17515dc5a9e8dee1317f9
Denis Obolensky <darthdenismaster@yandex.ru>
2022-06-21 10:55:40 +0300

    Add demo code

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AngleSharp;
+
+var config = Configuration.Default.WithDefaultLoader();
+var address = "https://trudvsem.ru/opendata/datasets";
+var context = BrowsingContext.New(config);
+var document = await context.OpenAsync(address);
+var linkSelector = "#dataset-10 .table__wrapper .link";
+var link = document.QuerySelector(linkSelector);
+
+Console.WriteLine("Href shouldn't be null:");
+Console.WriteLine(link?.GetAttribute("href"));
